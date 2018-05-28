b7ba63b600ce3ff0212d2052d9ec8463ef71db7e
Levente Kurusa <levex@linux.com>
2018-05-28 01:37:29 +0100

    Add basic backend functionality
    Signed-off-by: Levente Kurusa <levex@linux.com>

--- lib/notify_me_web/controllers/page_controller.ex
+++ lib/notify_me_web/controllers/page_controller.ex
@@ -1,17 +1,55 @@
 defmodule NotifyMeWeb.PageController do
+  import Ecto.Query
   use NotifyMeWeb, :controller
+  alias NotifyMe.{Computer, Repo}
 
   def index(conn, _params) do
     render conn, "index.html"
   end
 
   def create_new_id(conn, _params) do
-    json conn, %{status: :ok, uuid: UUID.uuid4()}
+    uuid = UUID.uuid4()
+    Repo.insert(%Computer{name: "something", uuid: uuid, last: ""})
+    json conn, %{status: :ok, uuid: uuid}
   end
 
-  def send_notification(conn, %{"uuid" => uuid}) do
+  defp do_notif(uuid, msg) do
+    prev = Repo.one(from c in Computer, where: ilike(c.uuid, ^uuid))
+    if prev == nil do
+      :error
+    else
+      Repo.delete(prev)
+      Repo.insert(%Computer{name: "test", uuid: uuid, last: msg})
+      :ok
+    end
+  end
+
+  defp read_notif(uuid) do
+    prev = Repo.one(from c in Computer, where: ilike(c.uuid, ^uuid))
+    if prev == nil do
+      :error
+    else
+      #IO.inspect(prev)
+      {:ok, prev.last}
+    end
+  end
+
+  def get_notification(conn, %{"uuid" => uuid}) do
     case UUID.info(uuid) do
-      {:ok, _} -> json conn, %{status: :ok, uuid: uuid}
+      {:ok, _} -> case read_notif(uuid) do
+          {:ok, msg} -> json conn, %{status: :ok, msg: msg}
+          _ -> json conn, %{status: :error_unknown_uuid}
+        end
+      _ -> json conn, %{status: :error_bad_uuid}
+    end
+  end
+
+  def send_notification(conn, %{"msg" => msg, "uuid" => uuid}) do
+    case UUID.info(uuid) do
+      {:ok, _} -> case do_notif(uuid, msg) do
+          :ok -> json conn, %{status: :ok, uuid: uuid, msg: msg}
+          _ -> json conn, %{status: :error_unknown_uuid}
+        end
       _ -> json conn, %{status: :error_bad_uuid}
     end
   end
--- lib/notify_me_web/router.ex
+++ lib/notify_me_web/router.ex
@@ -2,11 +2,11 @@ defmodule NotifyMeWeb.Router do
   use NotifyMeWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug :accepts, ["html", "json"]
+    #plug :fetch_session
+    #plug :fetch_flash
+    #plug :protect_from_forgery
+    #plug :put_secure_browser_headers
   end
 
   pipeline :api do
@@ -18,7 +18,8 @@ defmodule NotifyMeWeb.Router do
 
     get "/", PageController, :index
     get "/create", PageController, :create_new_id
-    get "/send/:uuid", PageController, :send_notification
+    get "/check/:uuid", PageController, :get_notification
+    post "/send/:uuid", PageController, :send_notification
   end
 
   # Other scopes may use custom stacks.
